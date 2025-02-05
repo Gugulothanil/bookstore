92f54bf105abcacf0fd84dcf186bc4e4c7043dae
Anil <gugulothanil950@gmail.com>
2025-02-05 17:27:06 +0530

    update all the changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {Component} from "react"
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+import Home from "./components/Home";
+import BookList from "./components/BookList";
+import BookDetails from "./components/BooksDetails";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import NotFound from "./components/NotFound";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <BrowserRouter>
+      
+      <Routes>
+        <Route exact  path ="/" element={<Home />}/>
+        <Route exact path ="/books" element={<BookList />}/>
+        <Route exact path ="/books/:id" element={<BookDetails />}/>
+        <Route exact  path ="/cart" element={<Cart />}/>
+        <Route exact path ="/checkout" element={<Checkout />}/>
+        <Route exact path ="/not-found" element={<NotFound />}/>
+        
+      </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
